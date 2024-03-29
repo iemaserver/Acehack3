6b54f9c85c6893b86f110dd9866aed23a22f3be2
bimlesharma <bimlesh.mdb@gmail.com>
2024-03-29 23:16:27 +0530

    mentor section completed

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -21,9 +21,12 @@ import rishika from "/public/assets/images/mentors3/Rishika.jpeg";
 import rohan from "/public/assets/images/mentors3/rohan.jpeg";
 import shambhavi from "/public/assets/images/mentors3/shambhavi.jpeg";
 import shivam from "/public/assets/images/mentors3/shivam.jpeg";
+import srishti from "/public/assets/images/mentors3/srishti.jpeg";
+import aryan from "/public/assets/images/mentors3/aryan.jpeg";
 import shyamal from "/public/assets/images/mentors3/shyamal.jpeg";
 import sounak from "/public/assets/images/mentors3/sounak.jpeg";
 import sunchit from "/public/assets/images/mentors3/sunchit.jpeg";
+import abhimanyu from "/public/assets/images/mentors3/abhimanyu.jpeg";
 
 
 import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
@@ -33,9 +36,37 @@ const Speakers = () => {
     <div className="relative items-center justify-center bg-gray-900/30 backdrop-blur-lg w-full z-20 py-10 scroll-smooth">
       <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div className="flex flex-row justify-center w-full">
-          <h1 className="grad-text font-bold text-5xl mb-10">Past Mentors</h1>
+          <h1 className="grad-text font-bold text-5xl mb-10">Mentors & Judges</h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <Member
+            image={sunchit}
+            name="Sunchit Dudeja"
+            social={
+              <a
+                href="https://www.linkedin.com/in/sunchitdudeja/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Mentor, Judge"
+          />
+          <Member
+            image={aryan}
+            name="Aryan Singh"
+            social={
+              <a
+                href="https://www.linkedin.com/in/singh1aryan/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Mentor & Judge"
+          />
           <Member
             image={abdallalit}
             name="Abdal Lalit"
@@ -66,7 +97,7 @@ const Speakers = () => {
           />
           <Member
             image={abhinav}
-            name="Abhinav S"
+            name="Abhinav S."
             social={
               <a
                 href="https://www.linkedin.com/in/abhinavsharma0/"
@@ -223,7 +254,7 @@ const Speakers = () => {
             name="Rishika Gupta"
             social={
               <a
-                href="https://www.linkedin.com/in/rishikagupta-rg/?originalSubdomain=in"
+                href="https://www.linkedin.com/in/rishikagupta-rg/"
                 className="text-2xl text-gray-400 pb-1"
                 target={"_blank"}
               >
@@ -232,6 +263,162 @@ const Speakers = () => {
             }
             // role="Mentor, Judge"
           />
+          <Member
+            image={rohan}
+            name="Rohan Kumar"
+            social={
+              <a
+                href="https://www.linkedin.com/in/rohan-kumar-a65a87175/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Mentor, Judge"
+          />
+          <Member
+            image={shivam}
+            name="Shivam Goyal"
+            social={
+              <a
+                href="https://www.linkedin.com/in/shivamgoyall/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Mentor, Judge"
+          />
+          <Member
+            image={srishti}
+            name="Srishti Gupta"
+            social={
+              <a
+                href="https://www.linkedin.com/in/srishti23/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Mentor, Judge"
+          />
+          
+          
+          <Member
+            image={shambhavi}
+            name="Shambhavi Gupta"
+            social={
+              <a
+                href="https://www.linkedin.com/in/shambhavigupta03/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Mentor, Judge"
+          />
+          <Member
+            image={shyamal}
+            name="Shyamal Tripathi"
+            // social={
+            //   <a
+            //     href="#"
+            //     className="text-2xl text-gray-400 pb-1"
+            //     target={"_blank"}
+            //   >
+            //     <FaLinkedin />
+            //   </a>
+            // }
+            role="Hackquest"
+          />
+          <Member
+            image={raajessh}
+            name="Rajeesh Kaashyap"
+            // social={
+            //   <a
+            //     href="#"
+            //     className="text-2xl text-gray-400 pb-1"
+            //     target={"_blank"}
+            //   >
+            //     <FaLinkedin />
+            //   </a>
+            // }
+            role="Nordek"
+          />
+          <Member
+            image={maninder}
+            name="Maninder Singh"
+            social={
+              <a
+                href="https://www.linkedin.com/in/manindersinghh/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Nordek"
+          />
+          <Member
+            image={abhimanyu}
+            name="Abhimanyu Shekhawat"
+            social={
+              <a
+                href="https://www.linkedin.com/in/abhimanyu-shekhawat-886896168/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Nordek"
+          />
+          <Member
+            image={devendra}
+            name="Devendra P."
+            social={
+              <a
+                href="https://www.linkedin.com/in/devendra-parihar/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Nordek"
+          />
+          <Member
+            image={sounak}
+            name="Sounak Ghosh"
+            social={
+              <a
+                href="https://www.linkedin.com/in/sounak-ghosh/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Nordek"
+          />
+          {/* <Member
+            image={sounak}
+            name="Saksham"
+            social={
+              <a
+                href="https://www.linkedin.com/in/sounak-ghosh/"
+                className="text-2xl text-gray-400 pb-1"
+                target={"_blank"}
+              >
+                <FaLinkedin />
+              </a>
+            }
+            // role="Nordek"
+          /> */}
         </div>
       </section>
     </div>
