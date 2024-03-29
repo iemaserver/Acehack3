3f1cf41f40dd65b8638cacbc2ef2e5df6c412730
Alok Agarwal <alokagarwal629@gmail.com>
2024-03-29 16:32:01 +0530

    Update Timeline.jsx
    hackathon timeline update

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -3,21 +3,21 @@ import React, { Fragment } from "react";
 const Timeline = () => {
   const events = [
     //Day 1
-    { direction: "right", time: "09:30 AM", heading: "Check-in" },
+    { direction: "right", time: "09:30 AM", heading: "Check-in Starts" },
     { direction: "left", time: "11:00 AM", heading: "Opening Ceremony" },
     { direction: "right", time: "11:30 AM", heading: "Rajasthan Police Session" },
-    { direction: "left", time: "12:00 PM", heading: "Hackathon starts" },
+    { direction: "left", time: "12:00 PM", heading: "Hackathon Starts" },
     { direction: "right", time: "01:30 PM", heading: "Lunch" },
     { direction: "left", time: "02:30 PM", heading: "Nordek Session" },
     { direction: "right", time: "03:30 PM", heading: "Orkes Session" },
-    { direction: "left", time: "04:10 PM", heading: "Hackquest Session" },
-    { direction: "right", time: "06:30 PM", heading: "Funzone opens" },
+    { direction: "left", time: "04:10 PM", heading: "HackQuest Session" },
+    { direction: "right", time: "06:30 PM", heading: "FunZone Opens" },
     { direction: "left", time: "07:30 PM", heading: "Tech Talk by Sunchit Dudeja" }, // Added event
-    { direction: "right", time: "08:30 PM", heading: "Dinner + Acehack3.0 unplugged" },
-    { direction: "left", time: "09:30 PM", heading: "Tech Talk by Aryan Singh" }, // Updated time
-    { direction: "right", time: "11:30 PM", heading: "Mentoring Round 1" },
-    { direction: "left", time: "12:00 AM", heading: "Midnight Snacks" },
-    { direction: "right", time: "12:30 AM", heading: "Game Zone" },
+    { direction: "right", time: "08:30 PM", heading: "Dinner + Acehack3.0 UnPlugged" },
+    { direction: "left", time: "11:30 PM", heading: "Tech Talk by Aryan Singh" }, // Updated time1
+    { direction: "right", time: "12:30 PM", heading: "Mentoring Round 1" },
+    { direction: "left", time: "01:00 AM", heading: "Midnight Snacks" },
+    { direction: "right", time: "01:30 AM", heading: "Midnight Games" },
 ];
 
     const events2 = [
@@ -25,7 +25,7 @@ const Timeline = () => {
         { direction: "right", time: "08:00 AM", heading: "Pulse Pump" },
         { direction: "left", time: "08:30 AM", heading: "Breakfast" },
         { direction: "right", time: "09:30 AM", heading: "Mentoring Round 2" },
-        { direction: "left", time: "01:00 PM", heading: "Hackathon Submission (Devfolio)" },
+        { direction: "left", time: "01:00 PM", heading: "Project Submission (Devfolio)" },
         { direction: "right", time: "01:30 PM", heading: "Lunch" },
         { direction: "left", time: "02:30 PM", heading: "Judging Starts" },
         { direction: "right", time: "06:00 PM", heading: "Closing Ceremony" },
